30db4ae94428ab8452ac154a234f2fedf412d97c
Wisnu <wisnuaryadipa93@gmail.com>
2022-06-07 10:42:01 +0700

    rename jquery directory in plugins

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -1,6 +1,6 @@
 
     <!-- jQuery 2.1.4 -->
-<script src="<?php echo base_url('');?>/assets/plugins/jQuery/jQuery-2.1.4.min.js"></script>
+<script src="<?php echo base_url('');?>/assets/plugins/jquery/jQuery-2.1.4.min.js"></script>
 <!-- Bootstrap 3.3.5 -->
 <script src="<?php echo base_url('/assets/plugins/bootstrap/js');?>/bootstrap.min.js"></script>
 <!-- FastClick -->
