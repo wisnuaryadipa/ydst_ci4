45a0b10787b9cda2d2575e7c3cafa7d447c1bdfe
Macbook Air <wisnuaryadipa93@gmail.com>
2022-07-16 23:38:38 +0700

    modify user and role form into compact form

--- app/Views/admin/user/create.php
+++ app/Views/admin/user/create.php
@@ -12,7 +12,7 @@
 </section>
 
 <!-- Main content -->
-<section class="content">
+<section class="content" id="compact-form">
     <div class="row">
     <!-- left column -->
     <div class="col-md-8 col-xl-12">
@@ -29,57 +29,73 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="form-group">
-                            <label for="nip">ID User</label>
-                            <input autofocus="autofocus" type="text" id="id_user" class="form-control" name="id_user" placeholder="ID User">
-                            <small class="help-block"></small>
+                            <label for="nip" class="col-md-2">ID User</label>
+                            <div class="col-md-10">
+                                <input autofocus="autofocus" type="text" id="id_user" class="form-control" name="id_user" placeholder="ID User">
+                            </div>
                         </div>
                         <div class="form-group">
-                            <label for="nip">Usernamer</label>
+                            <label for="nip" class="col-md-2">Usernamer</label>
+                            <div class="col-md-10">
                             <input autofocus="autofocus" type="text" id="username" class="form-control" name="username" placeholder="Usernamer">
                             <small class="help-block"></small>
+</div>
                         </div>
                         <div class="form-group">
                             <label for="nip">Password</label>
+                            <div class="col-md-10">
                             <input autofocus="autofocus" type="password" id="password" class="form-control" name="password" placeholder="Password">
                             <small class="help-block"></small>
+</div>
                         </div>
                         <div class="form-group">
                             <label for="nama_dosen">Nama User</label>
+                            <div class="col-md-10">
                             <input type="text" class="form-control" name="nama_user" placeholder="Nama User">
                             <small class="help-block"></small>
+</div>
                         </div>
                         <div class="form-group">
                             <label for="email">Lokasi / Area</label>
+                            <div class="col-md-10">
                             <select name="area" id="area" class="form-control select2" style="width: 100%!important">
                                 <option value="" disabled selected>Area</option>
                             </select>
                             <small class="help-block"></small>
+</div>
                         </div>
                         <div class="form-group">
                             <label for="matkul">Bagian</label>
+                            <div class="col-md-10">
                             <select name="bagian" id="bagian" class="form-control select2" style="width: 100%!important">
                                 <option value="" disabled selected>Bagian</option>
                             </select>
                             <small class="help-block"></small>
+</div>
                         </div>
                         <div class="form-group">
                             <label for="matkul">Jabatan</label>
+                            <div class="col-md-10">
                             <select name="jabatan" id="jabatan" class="form-control select2" style="width: 100%!important">
                                 <option value="" disabled selected>Jabatan</option>
                             </select>
                             <small class="help-block"></small>
+</div>
                         </div>
                         <div class="form-group">
                             <label for="matkul">Pendidikan</label>
+                            <div class="col-md-10">
                             <select name="pendidikan" id="pendidikan" class="form-control select2" style="width: 100%!important">
                                 <option value="" disabled selected>Pendidikan</option>
                             </select>
                             <small class="help-block"></small>
+</div>
                         </div>
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="matkul">Bergabung</label>
+                            <div class="col-md-10">
                                     <input type="text" class="form-control datepicker" name="bergabung" placeholder="Bergabung">
                                     <small class="help-block"></small>
                                 </div>
@@ -87,6 +103,7 @@
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="matkul">Berhenti</label>
+                            <div class="col-md-10">
                                     <input type="text" class="form-control datepicker" name="berhenti" placeholder="Berhenti">
                                     <small class="help-block"></small>
                                 </div>
